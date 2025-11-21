e82c761ac13fa6b6a5cadc0eee9c1983dfe0d443
André <teamworkall247@gmail.com>
2025-11-21 22:32:33 +0100

    update

--- src/app/components/providers/toast-provider.tsx
+++ src/app/components/providers/toast-provider.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+// testing
 import React, { createContext, useCallback, useContext, useState } from "react";
 
 type Toast = { id: string; message: string; type?: "info" | "error" | "success" };
