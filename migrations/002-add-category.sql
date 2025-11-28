61f3c0a7ca25de985e685cf65a921c0dfec1d495
André <teamworkall247@gmail.com>
2025-11-28 14:47:26 +0100

    update

--- migrations/002-add-category.sql
+++
@@ -1,3 +0,0 @@
--- Add category column to posts if it doesn't exist
-ALTER TABLE public.posts
-  ADD COLUMN IF NOT EXISTS category text;
--- migrations/003-remove-title-unique.sql
+++
@@ -1,2 +0,0 @@
--- Remove unique constraint on title column to allow duplicate titles
-ALTER TABLE IF EXISTS posts DROP CONSTRAINT IF EXISTS "Posts_title_key";
